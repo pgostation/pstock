57fefe678603085180f4dca642cf8d8aa5a4e2b0
pgo <waffle@tg7.so-net.ne.jp>
2016-01-21 22:20:49 +0900

    Initial Commit

---
+++ pstock/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  pstock
+//
+//  Created by takayoshi on 2016/01/21.
+//  Copyright © 2016年 pgostation. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
